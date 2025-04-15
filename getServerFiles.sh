d083245c2aff4a79be131ddbb657b9ddc0c6e8d4
Matdav001 <105984884+Matdav001@users.noreply.github.com>
2025-04-15 08:16:33 -0300

    fix(server): Whitelist and Auth
    * Enabled Whitelist.
    * Added Auth mod for testing.

--- getServerFiles.sh
+++ getServerFiles.sh
@@ -1,7 +1,8 @@
-wget -O ./gtnh-server-files/gtnh-2.7.4.zip https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.7.4_Server_Java_17-21.zip && \
-	wget -O ./gtnh-server-files/mods/forgeessentials-1.7.10-7.5.1-server.jar https://github.com/ForgeEssentials/ForgeEssentials/releases/download/7.5.1/forgeessentials-1.7.10-7.5.1-server.jar && \
-	mkdir -p ./mc-data/mods/ && \
-	cp ./gtnh-server-files/mods/forgeessentials-1.7.10-7.5.1-server.jar ./mc-data/mods/forgeessentials-1.7.10-7.5.1-server.jar && \
-	mkdir -p ./mc-data/ForgeEssentials/ && \
-	cp ./gtnh-server-files/mods/configs/main.cfg ./mc-data/ForgeEssentials/main.cfg
-	cp ./gtnh-server-files/mods/configs/AuthLogin.cfg ./mc-data/ForgeEssentials/AuthLogin.cfg
+if [ ! -f ./gtnh-server-files/gtnh-2.7.4.zip ]; then 
+	wget -O ./gtnh-server-files/gtnh-2.7.4.zip https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.7.4_Server_Java_17-21.zip
+fi
+if [ ! -f ./gtnh-server-files/mods/seamlessauth-1.3.1.jar ]; then 
+	wget -O ./gtnh-server-files/mods/seamlessauth-1.3.1.jar https://github.com/lubinacourec/SeamlessAuth/releases/download/1.3.1/seamlessauth-1.3.1.jar
+	mkdir -p ./mc-data/mods/
+	cp ./gtnh-server-files/mods/seamlessauth-1.3.1.jar ./mc-data/mods/seamlessauth-1.3.1.jar
+fi
