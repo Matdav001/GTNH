075ee0430422d3bf70854e5b29f49664858aa956
Matdav001 <105984884+Matdav001@users.noreply.github.com>
2025-04-16 11:41:18 -0300

    feat(server) Shared Ore Veins.
    * Added mod to server to share ore veins found by players in the same
    team.

--- getServerFiles.sh
+++ getServerFiles.sh
@@ -1,8 +1,10 @@
 if [ ! -f ./gtnh-server-files/gtnh-2.7.4.zip ]; then 
 	wget -O ./gtnh-server-files/gtnh-2.7.4.zip https://downloads.gtnewhorizons.com/ServerPacks/GT_New_Horizons_2.7.4_Server_Java_17-21.zip
 fi
-if [ ! -f ./gtnh-server-files/mods/seamlessauth-1.3.1.jar ]; then 
-	wget -O ./gtnh-server-files/mods/seamlessauth-1.3.1.jar https://github.com/lubinacourec/SeamlessAuth/releases/download/1.3.1/seamlessauth-1.3.1.jar
+if [ ! -f ./mc-data/mods/seamlessauth-1.3.1.jar ]; then 
 	mkdir -p ./mc-data/mods/
-	cp ./gtnh-server-files/mods/seamlessauth-1.3.1.jar ./mc-data/mods/seamlessauth-1.3.1.jar
+	wget -O ./mc-data/mods/seamlessauth-1.3.1.jar https://github.com/lubinacourec/SeamlessAuth/releases/download/1.3.1/seamlessauth-1.3.1.jar
+fi
+if [ ! -f ./mc-data/mods/sharedprospecting-2.0.2.jar ]; then 
+	wget -O ./mc-data/mods/sharedprospecting-2.0.2.jar https://github.com/Lyfts/SharedProspecting/releases/download/2.0.2/sharedprospecting-2.0.2.jar 
 fi
