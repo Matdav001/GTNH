541e63269404279cd3cb2f38fb146a66c70d7914
Matdav001 <105984884+Matdav001@users.noreply.github.com>
2025-08-09 13:10:50 -0300

    fix(sync) Add dedupe.
    * Clear duplicate files.

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rclone sync --delete-before /game/GTNH/mc-data/backups/ GTNH:/GTNH; rclone cleanup GTNH:
+rclone dedupe --dedupe-mode newest GTNH:/GTNH; rclone sync --delete-before /game/GTNH/mc-data/backups/ GTNH:/GTNH; rclone cleanup GTNH:
